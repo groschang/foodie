d5fd45328882fbe2229a9d3c6f796b94ab4d5117
Konrad Groschang <groschang.konrad@gmail.com>
2023-01-12 13:24:13 +0100

    Initial Commit

---
+++ drink/drinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  drinkApp.swift
+//  drink
+//
+//  Created by Konrad Groschang on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct drinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
