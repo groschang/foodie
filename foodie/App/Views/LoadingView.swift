1ac62166149f9829da913bed7383be8444f7595e
Konrad Groschang <groschang.konrad@gmail.com>
2025-09-17 18:35:37 +0200

    Loading View improvements

--- foodie/App/Views/LoadingView.swift
+++ foodie/App/Views/LoadingView.swift
@@ -14,17 +14,46 @@ struct LoadingView: View {
     
     var body: some View {
         VStack(spacing: 50) {
-
-            Text("Loading")
-                .modifier(TextStyle.infoScreenTitle)
-                .scaleEffect(textScale)
-                .animateForever(using: .easeInOut(duration: 2) ) {
-                    textScale = 1.3
-                }
-            
+            ModernCircularLoader()
         }
         .maxSize()
         .animatedGradient()
+    }
+}
+
+
+struct ModernCircularLoader: View {
+    @State private var trimEnd = 0.6
+    @State private var animate = false
+
+    var body: some View {
+        Circle()
+            .trim(from: 0.0,to: trimEnd)
+            .stroke(
+                .black,
+                style: StrokeStyle(
+                    lineWidth: 7,
+                    lineCap: .round,
+                    lineJoin:.round
+                )
+            )
+            .animation(
+                Animation.easeIn(duration: 1.5)
+                    .repeatForever(autoreverses: true),
+                value: trimEnd
+            )
+            .frame(width: 70,height: 70)
+            .rotationEffect(Angle(degrees: animate ? 270 + 360 : 270))
+            .animation(
+                Animation.linear(duration: 1)
+                    .repeatForever(autoreverses: false),
+                value: animate
+            )
+            .onAppear{
+                animate = true
+                trimEnd = 0
+            }
+            .opacity(0.6)
     }
 }
 
